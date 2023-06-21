ee26d36fee0079daca72f566925a7d7a8301afdd
Luka <luka.zivkovic007@gmail.com>
2023-06-22 00:15:32 +0200

    commit2

--- DragonGame.java
+++ DragonGame.java
@@ -44,12 +44,33 @@ public class DragonGame {
         if (playerHealth < 100 && playerHealth > 0) {
             pozHealth = poz(); // Priklic zdravljenja
             playerHealth += pozHealth; // Pristeje k tvojemu healthu
-            System.out.println("\nPozdravil si se in pridobil " + pozHealth + " health");
+            if (playerHealth > 100) {
+                while (playerHealth == 100) {
+                    playerHealth -= 1;
+                }
+                ;
+            }
             System.out.println("\n" + playerHealthMsg + playerHealth);
         } else {
             System.out.println(CannotPozMsg);
         }
         return playerHealth;
+    }
+
+    public static void Help() {
+        System.out.println("\nCilj igre je, da premagaš zmaja brez da ti umres.");
+        System.out.println("\nZmaj ima 100 življenskih točk tako kot ti.");
+        System.out.println("\nUporabiš lahko več različnih napadov kot so:");
+        System.out.println("\n-Lahki napad (a)- naredi med 1 do 20 življenski točk,");
+        System.out.println(
+                "\n-Močni napad (m)- naredi med 30 do 90 življenski točk in sebi odvzeme nekaj življenskih točk,");
+        System.out.println(
+                "\n-Zdravljenje (h) - se lahko pozdraviš za med 20 do 75 življenskih točk, če si pod 100 življenskih točk.");
+    }
+
+    public static void Stat(int playerHealth, int dragonHealth) {
+        System.out.println("\nTvoje health: " + playerHealth);
+        System.out.println("\nZmajev health: " + dragonHealth);
     }
 
     public static void main(String[] args) {
@@ -71,10 +92,11 @@ public class DragonGame {
         playerName = Scanner.next();
         System.out.println(playerName + "\n");
         System.out.println("Vpisi svoj stil napada:");
-        System.out.println("a - Attack");
-        System.out.println("m - Mocan attack");
+        System.out.println("a - Lahki napad");
+        System.out.println("m - Močan napad");
         System.out.println("h - se pozdravis/heal");
         System.out.println("p - Pomoč");
+        System.out.println("s - Statistika");
 
         // Uporabnik vpise stil napada
         for (int i = 1;; i++) {
@@ -100,13 +122,16 @@ public class DragonGame {
                 playerHealth = Heal(playerHealth, pozHealth);
             } // Pomoč
             else if (option == 'p') {
-                System.out.println("\nCilj igre je, da premagaš zmaja brez da ti umres.");
+                Help();
+            } // Statistika
+            else if (option == 's') {
+                Stat(playerHealth, dragonHealth);
             } // Če vnesena črka ni nič od navedenih
             else {
                 System.out.println("\nVnesena crka ni pravilna.");
             }
             // Napad Zmaja če ne ni vnesena črka za pomoč
-            if (option != 'p') {
+            if (option != 'p' && option != 's') {
                 System.out.println("\n Zmajev napad:");
                 playerHealth -= dragonAttack();
                 System.out.println("\n- " + dragonAttack());
